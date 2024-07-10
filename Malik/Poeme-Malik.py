04f55baed60188d3bed7b4bdd13c097f30011806
Oussama Bououden <oussama74@gmail.com>
2024-07-10 11:17:55 -0400

    Update Poeme-Malik.py
    ajouter un vers de Albert Lozeau  l'Ame Solitaire

--- Malik/Poeme-Malik.py
+++ Malik/Poeme-Malik.py
@@ -3,8 +3,10 @@
 def afficher_poeme():
     ligne1 = "Dans le jardin silencieux où la brume se lève,"
     ligne2 = "Chante la lune pâle éclairant les rêves."
+    ligne3= "Mon cœur est maintenant ouvert comme une porte."
     print(ligne1)
     print(ligne2)
+    print(ligne3)
 
 # Appel de la fonction pour afficher le poème
 afficher_poeme()
