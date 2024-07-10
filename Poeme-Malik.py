36bd29a96165f1c1e5fc1e428046adcf37f31c25
l-Malik <lardja_malik@yahoo.fr>
2024-07-10 10:52:07 -0400

    Create Poeme-Malik.py
    test cours

---
+++ Poeme-Malik.py
@@ -0,0 +1,10 @@
+# fonction afficher un poème
+
+def afficher_poeme():
+    ligne1 = "Dans le jardin silencieux où la brume se lève,"
+    ligne2 = "Chante la lune pâle éclairant les rêves."
+    print(ligne1)
+    print(ligne2)
+
+# Appel de la fonction pour afficher le poème
+afficher_poeme()
