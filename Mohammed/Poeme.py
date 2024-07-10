edacdcaa776e3bbfca3f12d4f9f5d6b10df7a77f
mohammedg15 <mohammedgaoui92@outlook.com>
2024-07-10 11:26:59 -0400

    Update Poeme.py

--- Mohammed/Poeme.py
+++ Mohammed/Poeme.py
@@ -1,16 +1,2 @@
 #Poeme 
-Hiver, vous n’êtes qu’un vilain,
-Été est plaisant et gentil,
-En témoin de Mai et d’Avril
-Qui l’accompagnent soir et matin.
-
-Été revêt champs, bois et fleurs,
-De sa livrée de verdure
-Et de maintes autres couleurs
-Par l’ordonnance de Nature.
-.
-Mais vous, Hiver, trop êtes plein
-De neige, vent, pluie et grésil ;
-On vous doit bannir en exil.
-Sans point flatter, je parle plain :
-Hiver, vous n’êtes qu’un vilain.
+test test test
