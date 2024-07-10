3295cc26ef9f7736e8440469a7ffe8f75b031d0a
mohammedg15 <mohammedgaoui92@outlook.com>
2024-07-10 11:17:05 -0400

    Update Poeme.py
    Hiver, vous n’êtes qu’un vilain, poeme de Charles d’Orléans

--- Mohammed/Poeme.py
+++ Mohammed/Poeme.py
@@ -1,5 +1,16 @@
 #Poeme 
-Test test 
+Hiver, vous n’êtes qu’un vilain,
+Été est plaisant et gentil,
+En témoin de Mai et d’Avril
+Qui l’accompagnent soir et matin.
 
-hahahahahaha
-blablabla
+Été revêt champs, bois et fleurs,
+De sa livrée de verdure
+Et de maintes autres couleurs
+Par l’ordonnance de Nature.
+  
+Mais vous, Hiver, trop êtes plein
+De neige, vent, pluie et grésil ;
+On vous doit bannir en exil.
+Sans point flatter, je parle plain :
+Hiver, vous n’êtes qu’un vilain.
