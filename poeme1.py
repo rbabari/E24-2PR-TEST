236fa48bc24a934ee7beb44ca7ecde4706519ea2
Oussama Bououden <oussama74@gmail.com>
2024-07-10 10:59:38 -0400

    Update poeme1.py

--- poeme1.py
+++ poeme1.py
@@ -2,4 +2,4 @@
 print(" Le ciel est bleu") 
 print(" ... La vie est belle ")
 print(" Que du bonheur ... ")
-print(".. qlqs malheurs pour se souvenir...")
+print(".. quelques eclats de rires pour se souvenir...")
